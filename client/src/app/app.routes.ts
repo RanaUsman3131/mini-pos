791a18e0fce46930d2e6c966407f8a65c2408b83
Rana Usman <usman.khan@bhomes.com>
2025-12-15 13:17:31 +0500

    update env

--- client/src/app/app.routes.ts
+++ client/src/app/app.routes.ts
@@ -2,7 +2,7 @@ import { Routes } from '@angular/router';
 import { PosPageComponent } from './pos/pos-page.component';
 
 export const routes: Routes = [
-  { path: '', redirectTo: 'pos', pathMatch: 'full' },
-  { path: 'pos', component: PosPageComponent },
-  { path: '**', redirectTo: 'pos' },
+  { path: '', redirectTo: '', pathMatch: 'full' },
+  { path: '', component: PosPageComponent },
+  { path: '**', redirectTo: '' },
 ];
